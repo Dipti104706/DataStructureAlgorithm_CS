4d1475f614d461106b4ad0a056116f4f1bafcaf5
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-27 01:02:00 +0530

    [Dipti] added code for sorting a list by using bubble sort

---
+++ DataStructureAlgorithm_CS/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm_CS
+{/// <summary>
+/// Template for sorting a list by using bubble sort
+/// </summary>
+    class BubbleSort
+    {
+        //Method for taking user input as a list of integer to sort them
+        public void UserInput()
+        {
+            int[] list = { 25, 1, 5, 9, 8, 75, 65, 8 };
+            SortList(list);
+        }
+
+        //Method for sorting of the given array of elements 
+        public void SortList(int[] list)
+        {
+            for (int i = 0; i < list.Count(); i++)
+            {
+                for (int j = i; j < list.Count(); j++)
+                    if (list[i] > list[j])
+                    {
+                        int temp =list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+            }
+            Console.WriteLine("The sorted array is:");
+            display(list);
+        }
+
+        //method for print the list after sorting
+        public void display(int[] list)
+        {
+            foreach (var element in list)
+            {
+                Console.Write(element + "\n");
+            }
+        }
+    }
+}
